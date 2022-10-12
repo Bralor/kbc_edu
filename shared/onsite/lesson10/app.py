c70a7ab1bae9e64cfb7b93f53f7024fd71998e17
Matouš Holinka <bralor92@email.cz>
2022-10-12 11:38:40 +0200

    Demo3: Added a new demo.

--- shared/onsite/lesson10/app.py
+++ shared/onsite/lesson10/app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template("index.html")
+    return render_template("index.html", jmeno_stranky="DOMU")
 
 
 @app.route("/clanky")
 def topics():
-    return render_template("topics.html")
+    return render_template("topics.html", jmeno_stranky="CLANKY")
