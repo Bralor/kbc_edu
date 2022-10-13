75d6e1e77242ee9c218bce57747d5f527cafe77e
Matouš Holinka <bralor92@email.cz>
2022-10-13 10:18:36 +0200

    Demo4.5: Added the sample.

--- shared/onsite/lesson10/app.py
+++ shared/onsite/lesson10/app.py
@@ -1,6 +1,6 @@
 import csv
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -17,6 +17,28 @@ def topics():
         "topics.html",
         jmeno_stranky="CLANKY",
         clanky=topics
+    )
+
+
+@app.route("/clanky/<int:id>")
+def topic_details(id):
+    topics = read_topics("clanky.csv")
+
+    for topic in topics:
+        if topic["Id"] == str(id):
+            result = topic         
+            break
+
+        else:
+            result = None
+
+    if not result:
+        abort(404)
+
+    return render_template(
+        "topic.html",
+        jmeno_stranky=f"CLANEK {id}",
+        clanek=result
     )
 
 
