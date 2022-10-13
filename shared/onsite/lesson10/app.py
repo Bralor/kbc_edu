9ef10c9552cc07639c7ae4f95136bcbcf8a6e2f5
Matouš Holinka <bralor92@email.cz>
2022-10-13 08:00:16 +0200

    Demo4: Added the fourth sample.

--- shared/onsite/lesson10/app.py
+++ shared/onsite/lesson10/app.py
@@ -1,3 +1,5 @@
+import csv
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,4 +12,19 @@ def root():
 
 @app.route("/clanky")
 def topics():
-    return render_template("topics.html", jmeno_stranky="CLANKY")
+    topics = read_topics("clanky.csv")
+    return render_template(
+        "topics.html",
+        jmeno_stranky="CLANKY",
+        clanky=topics
+    )
+
+
+def read_topics(jmeno_souboru: str) -> list:
+    with open(jmeno_souboru, mode="r", encoding="utf-8") as csv_:
+        reader = csv.DictReader(csv_)
+
+        return [
+            radek
+            for radek in reader
+        ]
