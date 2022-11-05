1486187ea1bab724ea596f0e7091d71a631d29e6
Matouš Holinka <bralor92@email.cz>
2022-11-05 23:29:23 +0100

    Added tasks 06

---
+++ shared/onsite/lesson06/uloha_g2_5.py
@@ -0,0 +1,133 @@
+import random
+
+zamestnanci_raw = """
+Helena Vybíralová
+Wendy Štrumlová
+Marie Vybíralová
+Stanislav Bechyňka
+Zdeňka Urbánková
+Lukáš Riečan
+Veronika Koudelová
+Františka Vorlová
+Ilie Seleš
+Martin Železný
+Petra Niklesová
+Bohumil Skok
+Jakub Šmíd
+Jarmila Procházková
+Dagmar Hlavatá
+Jiří Nguyen Thanh
+Marie Franková
+Dana Ulrichová
+Jana Hranická
+Hana Budošová
+Ivan Široký
+Květoslava Jiráčková
+Pavel Przywara
+Josef Umlauf
+Tomáš Granzer
+Miroslav Kuba
+Miloslava Adámková
+Marie Karlíková
+Jaroslav Hronský
+Vlasta Karlíková
+Andrea Žatková
+Zuzana Lokočová
+Ondřej Ptáček
+Zdeněk Najman
+Tereza Šebešová
+Antonie Skokánková
+Jan Lion
+Václav Vecko
+František Vajgl
+Adéla Kavková
+Amália Vacková
+Anna Pažická
+Ivo Pustějovský
+Antonín Pavela
+Jitka Adamová
+Libuše Hamroziová
+Drahomíra Balzerová
+Marek Suchánek
+Petr Vavrinec
+Jonáš Stuchlý
+Jaromír Pecen
+Markéta Kyliánková
+Marina Pečenková
+Ivana Perdochová
+Michaela Drápalová
+Michael Mentlík
+Rudolf Špičák
+Žaneta Holá
+Blanka Lišková
+Eva Svatoňová
+Rostislav Hoang
+Martina Kalivodová
+Milan Hruška
+Zdenka Marková
+Lenka Schambergerová
+Růžena Martinů
+Věra Řezanková
+Marie Pečenková
+Miloš Váchal
+Jaroslava Hrubá
+Petr Pecen
+Pavla Konvicová
+Lucie Marešová
+Květuše Zdráhalová
+Vlastimila Svatošová
+Zora Michalčíková
+Daniel Švejnoha
+Klára Brunclíková
+Vladimír Bauer
+Michal Slaný
+Jiřina Novosadová
+Karel Sršeň
+Stanislava Lakosilová
+Filip Černý
+Alena Kubiková
+Sára Kotrlová
+Alois Rejlek
+Božena Novotná
+Maryana Nováková
+Kateřina Máslová
+Ladislav Dvořák
+Radek Varga
+Petr Dvořák
+Ludmila Jaklová
+Renáta Foubíková
+Nikola Lehká
+Dominika Riegerová
+Patrik Polák
+Soňa Štrbová
+David Matoušek
+Liubov Hollíková
+Monika Poláková
+Marie Jaklová
+Aleš Svoboda
+Roman Kolínský
+Karolína Košiková
+"""
+
+# Jak procházet jednotlivá celá jména
+# .. ne jenom písmena!
+for index, cele_jmeno in enumerate(zamestnanci_raw.split("\n")[1:-1], 1):  # Obdoba: .splitlines()
+    jmeno = cele_jmeno.split()[0]
+    prijmeni = cele_jmeno.split()[1]
+
+    # Vytvoř ID zaměstnance
+    id_zamestnance = index
+    # print(id_zamestnance)
+
+    # Vytvoř telefonní číslo
+    predcisli = "+420"  # str
+    devet_cisel = random.randrange(100000000, 999999999)  # int
+    telefonni_cislo = " ".join((
+        predcisli,
+        str(devet_cisel)[:3],
+        str(devet_cisel)[3:6],
+        str(devet_cisel)[6:]
+    ))
+    print(telefonni_cislo)
+
+
